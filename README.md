9351f80d40f475598b0a6f2cbe6acf76ee9ee91c
Nikolas <ZaL1Vv2003@gmail.com>
2023-09-19 16:35:25 +0200

    initial database structure

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BarterBot
 Telegram bot for exchanging items
+
+
+## Database schema
+![DB schema](https://media.discordapp.net/attachments/1104017059425308672/1153596732161130516/image.png?width=943&height=671)
+
---
+++ database/db.py
@@ -0,0 +1,200 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, JSON, Float, DateTime, DECIMAL
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+
+Base = declarative_base()
+engine = create_engine('sqlite:///db.sqlite3', echo=True)
+
+
+class Country(Base):
+    __tablename__ = 'country'
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name_en = Column(String)
+    name_uk = Column(String)
+    name_pl = Column(String)
+    name_ru = Column(String)
+    
+    def __init__(
+        self, name_en, name_uk, name_pl,
+        name_ru
+        ):
+        self.name_en = name_en
+        self.name_uk = name_uk
+        self.name_pl = name_pl
+        self.name_ru = name_ru
+        super().__init__()
+
+
+class Region(Base):
+    __tablename__ = 'region'
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name_en = Column(String)
+    name_uk = Column(String)
+    name_pl = Column(String)
+    name_ru = Column(String)
+    country = Column(Integer, ForeignKey('country.id'))
+    
+    def __init__(self, name_en, name_uk, name_pl,
+        name_ru, name_tr, status, country):
+        self.name_en = name_en
+        self.name_uk = name_uk
+        self.name_pl = name_pl
+        self.name_ru = name_ru
+        self.name_tr = name_tr
+        self.status = status
+        self.country = country
+        super().__init__()
+
+
+class City(Base):
+    __tablename__ = 'city'
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name_en = Column(String)
+    name_uk = Column(String)
+    name_pl = Column(String)
+    name_ru = Column(String)
+    region = Column(Integer, ForeignKey('region.id'))
+    
+    def __init__(self, name_en, name_uk, name_pl,
+        name_ru, region
+        ):
+        self.name_en = name_en
+        self.name_ua = name_uk
+        self.name_pl = name_pl
+        self.name_ru = name_ru
+        self.region = region
+        super().__init__()
+
+
+class Location(Base):
+    __tablename__ = 'location'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    country = Column(Integer, ForeignKey('country.id'))
+    region = Column(Integer, ForeignKey('region.id'))
+    city = Column(Integer, ForeignKey('city.id'))
+    long_itude = Column(Float)
+    lat_itude = Column(Float)
+
+    def __init__(
+        self, country, region, city, long_itude, lat_itude 
+        ):
+        self.country = country
+        self.region = region
+        self.city = city
+        self. long_itude = long_itude
+        self.lat_itude = lat_itude
+        super().__init__()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    
+    telegram_id = Column(Integer, primary_key=True, autoincrement=False)
+    username = Column(String)
+    language = Column(String)
+    balance = Column(Float)
+    status = Column(String)
+    location = Column(Integer, ForeignKey('location.id'))
+    data = Column(JSON)
+    currency = Column(String)
+    rating = Column(JSON)
+    created_at = Column(DateTime)
+    
+    def __init__(
+        self, telegram_id, language,
+        status, data, rating,
+        location, username
+        ):
+        self.telegram_id = telegram_id
+        self.username = username
+        self.language = language
+        self.status = status
+        self.data = data
+        self.location = location
+        self.rating = rating
+        self.created_at = datetime.now()
+        super().__init__()
+
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    status = Column(String)
+    name_en = Column(String)
+    name_uk = Column(String)
+    name_pl = Column(String)
+    name_ru = Column(String)
+    
+    def __init__(self, status, name_en, name_uk, name_pl,
+        name_ru
+        ):
+        self.status = status
+        self.name_en = name_en
+        self.name_ua = name_uk
+        self.name_pl = name_pl
+        self.name_ru = name_ru
+        super().__init__()
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    user = Column(Integer, ForeignKey('user.telegram_id'))
+    name = Column(String)
+    media = Column(JSON)
+    description = Column(String)
+    status = Column(String)
+    created_at = Column(DateTime)
+
+    def __init__(
+        self, user, name, media, description, status 
+        ):
+        self.user = user
+        self.name = name
+        self.media = media
+        self.description = description
+        self.status = status
+        self.created_at = datetime.now()
+        super().__init__()
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    user = Column(Integer, ForeignKey('user.telegram_id'))
+    item = Column(Integer, ForeignKey('item.id'))
+    
+    def __init__(
+        self, user, item
+        ):
+        self.user = user
+        self.item = item
+        super().__init__()
+
+
+class ItemTag(Base):
+    __tablename__ = 'item_tag'
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    item = Column(Integer, ForeignKey('item.id'))
+    tag = Column(Integer, ForeignKey('tag.id'))
+    
+    def __init__(
+        self, item, tag
+        ):
+        self.item = item
+        self.tag = tag
+        super().__init__()
+
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(engine)
+session = Session()
